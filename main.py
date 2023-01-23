85a4f6b37c2733c8b80564d5112c687090b39071
kr1s7ian <kristianboy44@gmail.com>
2023-01-23 20:36:35 +0100

    first commit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import customtkinter as ck
 import subprocess as sp
-import os, signal
+import os
+import signal
 import toml
 import sys
 import tkinter
@@ -8,15 +9,21 @@ from pynput import keyboard
 from PIL import ImageTk
 from functools import partial
 
+
 def Steamkill():
     os.system("taskkill.exe /F /IM steam.exe")
 
+
 def Steamlogin(username):
-    os.system(F'reg add "HKCU\Software\Valve\Steam" /v AutoLoginUser /t REG_SZ /d '+username+' /f')
-    os.system(F'reg add "HKCU\Software\Valve\Steam" /v RememberPassword /t REG_DWORD /d 1 /f')
+    os.system(
+        F'reg add "HKCU\Software\Valve\Steam" /v AutoLoginUser /t REG_SZ /d '+username+' /f')
+    os.system(
+        F'reg add "HKCU\Software\Valve\Steam" /v RememberPassword /t REG_DWORD /d 1 /f')
+
 
 def Steamopen():
     os.system("start steam://open/main")
+
 
 def AccButton(username):
     Steamkill()
@@ -26,12 +33,13 @@ def AccButton(username):
         listener.stop()
         sys.exit()
 
+
 def loadconfig():
-    with open("config.toml","r") as f:
+    with open("config.toml", "r") as f:
         return toml.load(f)
 
 
-config=loadconfig()
+config = loadconfig()
 
 
 def keypress(key):
@@ -51,33 +59,34 @@ def keypress(key):
             print("test")
 
 
-listener=keyboard.Listener(on_press=keypress)
+listener = keyboard.Listener(on_press=keypress)
 listener.start()
 
 
 ck.set_appearance_mode("dark")
 ck.set_default_color_theme(R"Theme.json")
-root=ck.CTk()
+root = ck.CTk()
 root.geometry("600x500")
-root.resizable(0,0)
+root.resizable(0, 0)
 root.title("Steam Account Switcher By pog#5249")
 
 
-frame=ck.CTkFrame(master=root)
-frame.pack(pady=15,padx=15,fill="both",expand=True)
-label=ck.CTkLabel(master=frame,text="Steam Account Switcher",font=("Arialbd",40))
-label.pack(pady=20,padx=60)
+frame = ck.CTkFrame(master=root)
+frame.pack(pady=15, padx=15, fill="both", expand=True)
+label = ck.CTkLabel(
+    master=frame, text="Steam Account Switcher", font=("Arialbd", 40))
+label.pack(pady=20, padx=60)
 
 
 for account in config["accounts"]:
-    titolo=account[0]
-    username=account[1]
-    callback=partial(AccButton,username)
-    button=ck.CTkButton(master=frame,text=titolo,command=callback)
-    button.pack(pady=15,padx=6)
+    titolo = account[0]
+    username = account[1]
+    callback = partial(AccButton, username)
+    button = ck.CTkButton(master=frame, text=titolo, command=callback)
+    button.pack(pady=15, padx=6)
 
-CloseOnSwitch=ck.CTkSwitch(master=frame,text='Close on Switch')
+CloseOnSwitch = ck.CTkSwitch(master=frame, text='Close on Switch')
 CloseOnSwitch.toggle()
-CloseOnSwitch.pack(pady=10,padx=6)
+CloseOnSwitch.pack(pady=10, padx=6)
 
 root.mainloop()
